d2d83cf2a5279b5bf431e57ae8fb9e07d17552f8
Robel Fekadu <robelfekadu@gmail.com>
2022-10-08 14:18:35 +0300

    updated index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const cors = require('cors');
 
 dotenv.config();
 mongoose
@@ -10,6 +11,16 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+
+app.use(cors());
+app.use(express.json);
+//routes
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/users', require('./routes/user'));
+app.use('/api/products', require('./routes/product'));
+app.use('/api/carts', require('./routes/cart'));
+app.use('/api/orders', require('./routes/order'));
+app.use('/api/checkout', require('./routes/stripe'));
 
 app.listen(process.env.PORT || 5000, () => {
   console.log('Server running on!');
